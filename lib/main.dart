cf134ee4c5e8c62ecd0d027cb093fa3e260ab645
MuhammadSalmanMehmood <connect_salman@live.com>
2020-05-12 10:13:51 +0500

    Network Image Added

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[900],
-        title: Text('My First Flutter App'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('My First Flutter App'),
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://plugins.jetbrains.com/files/13666/85971/icon/META-INF_pluginIcon.png')),
+        ),
       ),
-      
-    )),
+    ),
   );
 }
