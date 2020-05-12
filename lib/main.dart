c20f08861a830b7a8110ff818b9f8d77e2e702ce
MuhammadSalmanMehmood <connect_salman@live.com>
2020-05-12 09:38:12 +0500

    Appbar and background color added

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ void main() {
   runApp(
     MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
         title: Text('My First Flutter App'),
       ),
+      
     )),
   );
 }
